accc942e6c054b2160af8ff7c8206ffa6307184b
Dharshini Natarajan <dharshininatarajan584@gmail.com>
2026-07-15 20:14:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ elif menu == "📄 Prescription Reader":
                 If something is unreadable write 'Unclear'. Do not guess."""
                 try:
                     response = client.models.generate_content(
-                        model="gemini-1.5-flash",
+                        model="gemini-1.5-flash-001",
                         contents=[prompt, image]
                     )
                     st.subheader("Extracted Prescription")
